db49f61468a06f472a7aeff3e404a9324e13fa44
adamservis <adam.m.servis@gmail.com>
2016-02-27 15:01:55 -0800

    Added route to process a matchID and return a JSON representation of the match timeline history.

--- apps.js
+++ apps.js
@@ -18,6 +18,7 @@ var getCurrentGameURL = 'https://na.api.pvp.net/observer-mode/rest/consumer/getS
 var getRecentMatches = 'https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/';
 var getMatchTimeLine = 'https://na.api.pvp.net/api/lol/na/v2.2/match/';
 var froQueue = [];
+var pickupMap = [];
 
 // to get summoner id
 // https://na.api.pvp.net/api/lol/{region}/v1.4/summoner/by-name/{summonerNames}?api_key=<key>
@@ -25,16 +26,11 @@ var froQueue = [];
 // timeline example
 //https://na.api.pvp.net/api/lol/na/v2.2/match/2106980098?includeTimeline=true&api_key=29b7f478-99c4-46af-b831-b5605e1b42af
 
-app.get("/test",function(req,res){
-	testVar++;
-	res.send("counter = " + testVar);       
-});
-
 app.get("/",function(req,res){
   res.send("<h1>Test</h1>");
 });
 
-app.listen(80, function(){
+app.listen(3000, function(){
  setInterval(function(){
    QueueHandler();
  }, 1000);
@@ -57,11 +53,12 @@ function QueueHandler(){
 		getRecentGamesForSummoner(froQueue[0]);
 		break;
 	case FETCH_TIMELINES:
-		console.log('at fetch timelines, current fro ' + JSON.stringify(froQueue[0])+ '\n\n');
+		//console.log('at fetch timelines, current fro ' + JSON.stringify(froQueue[0])+ '\n\n');
 		getTimeline(froQueue[0]);
 		break;
 	case FINISHED:
 		//remove fro from queue, add to id -> fro map
+		console.log('finished');//+ JSON.stringify(froQueue[0])+ '\n\n');
 		break;
   }
 }
@@ -74,6 +71,66 @@ app.get("/summoner/:summoner",function(req,res){
   froQueue.push(FRO);
 });
 
+app.get("/matchId/:match", function(req,res){
+	
+	//https://na.api.pvp.net/api/lol/na/v2.2/match/2106980098?includeTimeline=true&api_key=29b7f478-99c4-46af-b831-b5605e1b42af
+	var reqString = '';
+	reqString = getMatchTimeLine
+		  + req.params.match
+		  + '?includeTimeline=true&api_key='
+		  + riotApiKey;
+		var participantMap = {};
+		var req2 = https.request(reqString, function(APIRes) {
+			var JSONResponseString = '';
+			//APIRes.setEncoding('utf-8');
+
+			APIRes.on('data', function(data){
+				JSONResponseString += data;
+			});
+
+			APIRes.on('end', function(){
+			//console.log(JSONResponseString);
+				var JSONResponseObject = JSON.parse(JSONResponseString);
+				if(JSONResponseObject.status){
+					//failure
+					reportError(JSONResponseObject.status[Object.getOwnPropertyNames(JSONResponseObject.status)[1]]);
+				} else {
+					//success
+					//matchObject.timeLine =
+					var participantInformation = JSONResponseObject.participants;
+					
+					for(var i = 0; i < participantInformation.length; i++){
+						var property = '' + participantInformation[i].participantId;
+						participantMap[property] = { 'champId' : participantInformation[i].championId,
+													  'teamId' : participantInformation[i].teamId,
+													  'timeline' : {}};
+					};
+					
+					var summonerTimelineData = new Array(10);
+					var FrameList = JSONResponseObject.timeline.frames;
+					for(var i = 0; i < FrameList.length; i++){
+						var frame = FrameList[i];
+						//console.log('on frame ' + i + ' frame info ' + JSON.stringify(FrameList[i].participantFrames) + ' \n');
+						var time = Math.floor(frame.timestamp/60000);
+						console.log('time: ' + time);
+						for (var j = 1; j<11; j++) {
+							//console.log('prop info' + JSON.stringify(FrameList[i].participantFrames[j].totalGold) + ' \n');
+							//var timeProp = '' + time;
+							participantMap[j].timeline[time] = {'gold' : FrameList[i].participantFrames[j].totalGold,
+															  'xp'   : FrameList[i].participantFrames[j].xp};
+                        }
+					};
+					console.log('\n');
+					console.log(JSON.stringify(participantMap));
+					res.setHeader('Content-Type', 'application/json');
+					res.send(JSON.stringify(participantMap));
+				}
+			});
+		});
+		req2.end();
+		
+
+  });
 
 
 function getSummonerId(theFRO){
@@ -133,12 +190,12 @@ function getCurrentGameId(theFRO) {
 		
         for(var i = 0; i < participantArray.length; i++){
 	    //console.log('Summoner: ' + participantArray[i].summonerName + ' Id: ' + participantArray[i].summonerName );
-        var summonerData = { SummonerName : participantArray[i].summonerName,
-							 SummonerId : participantArray[i].summonerId,
-							 ChampId : participantArray[i].championId,
-							 teamId : participantArray[i].teamId };
+			var summonerData = { SummonerName : participantArray[i].summonerName,
+								SummonerId : participantArray[i].summonerId,
+								ChampId : participantArray[i].championId,
+								teamId : participantArray[i].teamId };
 							 
-		summonerDataForFRO[i] = summonerData;
+			summonerDataForFRO[i] = summonerData;
 		}
 		theFRO.summonerDataCurrentMatch = summonerDataForFRO;
 		//console.log('summonerId: ' + JSON.stringify(summonerDataForFRO) + ' being added to thefro');
@@ -146,8 +203,7 @@ function getCurrentGameId(theFRO) {
       }      
     });
   });
-  req.end();
-  
+  req.end();  
 }
 
 function getRecentGamesForSummoner(theFRO){
@@ -193,47 +249,72 @@ function getRecentGamesForSummoner(theFRO){
 				var recentMatchesWithCurrentChamp = [];
 				for(var i = 0; i < recentMatchList.length; i++){
 				//console.log('\n\nPlayer: ' + summonerData.SummonerName +' current champ Id: ' + summonerData.ChampId + ' history champ id: ' + recentMatchList[i].championId + '\n\n');
-					if(recentMatchList[i].championId == summonerData.ChampId){
+					//if(recentMatchList[i].championId == summonerData.ChampId){
 						//console.log('\n\nMatch found!!! id: '+ recentMatchList[i].gameId + '\n\n');
 						recentMatchesWithCurrentChamp.push({matchId : recentMatchList[i].gameId});
-					}
+					//}
 				}
-				summonerData.recentMatchesWithCurrentChamp = recentMatchesWithCurrentChamp;
+				summonerData.recentMatches = recentMatchesWithCurrentChamp;
 				summonerData.recentMatchLoadComplete = true;
 			}
 		});
 	});
-req.end();
+	req.end();
 }
 
 function getTimeline(theFRO) {
-	if (theFRO.summonerForTimeline > 9) {
-        //We're done!
-		theFRO.state = FINISHED;
-		return;
-    }
-
+	
 	//find summoner data we are working with
-	var matchesToFetch = theFRO.summonerDataCurrentMatch[theFRO.summonerForTimeline].recentMatchesWithCurrentChamp;
+	var findingMatches = true;
+	var matchesToFetch;
+
+	while (findingMatches) {
+		if (theFRO.summonerForTimeline > 9) {
+			//We're done!
+			theFRO.state = FINISHED;
+			return;
+		}
+		matchesToFetch = theFRO.summonerDataCurrentMatch[theFRO.summonerForTimeline].recentMatches;
+		if (matchesToFetch.length == 0) {
+            //no matches to find, check next summoner
+			theFRO.summonerForTimeline++;
+        } else {
+			//we found a summoner with matches
+			//
+			findingMatches = false;
+		}
+	}
+    
+	console.log('Getting matches for summoner: ' + theFRO.summonerForTimeline
+				+ ' Total matches ' + matchesToFetch.length + '\n');
 	
 	var matchObject;
-	for(var i = 0; i < matchesToFetch; i++){
+	var continueToNextSummoner = true;
+	for(var i = 0; i < matchesToFetch.length; i++){
 		if (!matchesToFetch[i].timeLine && !matchesToFetch[i].startedTimeLineRetrieval) {
             //we found a match without a timeline
+			console.log('fetching match: ' + i + '\n');
 			matchObject = matchesToFetch[i];
 			//flag start of timeline download
 			matchObject.startedTimeLineRetrieval = true;
+			continueToNextSummoner = false;
+			break;
         }
+	}
+	
+	if(continueToNextSummoner){
+		theFRO.summonerForTimeline++;
+		return;
 	}
 	
 	//https://na.api.pvp.net/api/lol/na/v2.2/match/2106980098?includeTimeline=true&api_key=29b7f478-99c4-46af-b831-b5605e1b42af
 	var reqString = '';
 	reqString = getMatchTimeLine
-		  + matchId
+		  + matchObject.matchId
 		  + '?includeTimeline=true&api_key='
 		  + riotApiKey;
 	
-	var req = https.request(reqString, function(APIRes) {
+		var req = https.request(reqString, function(APIRes) {
 		var JSONResponseString = '';
 		//APIRes.setEncoding('utf-8');
 
@@ -249,13 +330,16 @@ function getTimeline(theFRO) {
 			reportError(JSONResponseObject.status[Object.getOwnPropertyNames(JSONResponseObject.status)[1]]);
 		} else {
 			//success
-			matchObject.timeLine = 
+			//matchObject.timeLine =
+			console.log("Match id: " + matchObject.matchId + '\n\n');
+			
 		}
 		
     });
   });
   req.end();
 }
+
 
 function reportError(error) {
     switch(error){
